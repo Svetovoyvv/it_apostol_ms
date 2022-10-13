a27ae3f117a6b4ce0634aec42f21b1a55a35a3e4
CookiesWithMoloko <95980525+CookiesWithMoloko@users.noreply.github.com>
2022-10-14 00:13:49 +0300

    1

--- api/crud/people/__init__.py
+++ api/crud/people/__init__.py
@@ -1,7 +1,11 @@
 from ..models.people import PeopleModel
 from sqlalchemy.orm import Session
 from .models import *
+from sqlalchemy import or_
 class PeopleCRUD:
     @classmethod
     def get_by_ins_number(cls, db: Session, ins_number: str) -> list[PeoplePublic] | None:
-        return db.query(PeopleModel).filter(PeopleModel.ins_number == ins_number).all()
+        return db.query(PeopleModel).filter(or_(
+            PeopleModel.ins_number == ins_number,
+            PeopleModel.ins_number == ins_number.replace(' ', '-'),
+        )).all()
