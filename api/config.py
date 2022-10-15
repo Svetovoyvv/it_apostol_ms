24db9b41034b328e147dc90602bc58478e0e49ce
CookiesWithMoloko <95980525+CookiesWithMoloko@users.noreply.github.com>
2022-10-15 11:38:04 +0300

    1

--- api/config.py
+++ api/config.py
@@ -1,4 +1,4 @@
 import os
 JWT_SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret'
 CORS_FRONTEND_URL = (os.environ.get('CORS_FRONTEND_URL') and os.environ.get('CORS_FRONTEND_URL').split()) or ['http://localhost:3000']
-ACCESS_TOKEN_EXPIRE_SECONDS = os.environ.get('ACCESS_TOKEN_EXPIRE_SECONDS') or 24 * 60 * 60
+ACCESS_TOKEN_EXPIRE_SECONDS = int(os.environ.get('ACCESS_TOKEN_EXPIRE_SECONDS')) or 24 * 60 * 60
